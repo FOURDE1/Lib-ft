37e5ca84ca87d1115a0b1b65977898b2ed620efd
Hussein Raad <hraad@c1r9s6.42beirut.com>
2024-08-20 21:34:03 +0300

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,35 +6,40 @@
 /*   By: hraad <hraad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 00:39:24 by hraad             #+#    #+#             */
-/*   Updated: 2024/08/19 00:58:01 by hraad            ###   ########.fr       */
+/*   Updated: 2024/08/20 15:57:17 by hraad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-int	:static isfound()
+static	int	isfound(char c, char const *set);
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
 	char	*ptr;
-	size_t	size;
-	size_t	start;
-	size_t	end;
+	char	*start;
+	char	*end;
 
 	if (!s1 || !set)
 		return (NULL);
-	size = ft_strlen(s1);
-	if (size == 0)
-	{
-		ptr = (char *)malloc(1 * sizeof(char));
-		ptr[0] = '\0';
-		return (ptr);
-	}
-	if (ft_strlen(set) == 0)
-	{
-		ptr = (char *)malloc((size + 1) * sizeof(char));
-		ft_strlcpy(ptr, s1, (size + 1));
-		return (ptr);
-	}
+	start = (char *)s1;
+	end = start + isfound(s1);
+	while (*start && isfound(*start, set))
+		++start;
+	while (start < end && isfound(*(end - 1), set))
+		--end;
+	ptr = ft_substr(start, 0, end - start);
+	return (ptr);
+}
+
+static	int	isfound(char c, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i] != '\0')
+		if (c == set[i++])
+			retrun (1);
+	return (0);
 }
