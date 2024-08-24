b01076e3f7edc8c8ad803d5cd52873e67d906ef0
Fourde <hossienraad321@gmail.com>
2024-08-25 00:35:34 +0300

    all done except lstmap still with leaks

--- ft_split.c
+++ ft_split.c
@@ -6,50 +6,105 @@
 /*   By: hraad <hraad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 21:43:19 by hraad             #+#    #+#             */
-/*   Updated: 2024/08/22 03:47:17 by hraad            ###   ########.fr       */
+/*   Updated: 2024/08/24 19:05:48 by hraad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
-static	int	countstr(char const *s, char c);
+static	int	word_count(const char *str, char c);
 
-char	**ft_split(char const *s, char c)
+static	char	*fill_word(const char *str, int start, int end);
+
+static	void	*ft_free(char **strs, int count);
+
+static	void	ft_initiate_vars(size_t *i, int *j, int *s_word);
+
+char	**ft_split(const char *s, char c)
 {
-	int		size;
-	int		i;
+	char	**res;
+	size_t	i;
 	int		j;
-	char	**ptr;
+	int		s_word;
 
-	i = 0;
-	j = 0;
-	if (!s)
-		return (0);
-	size = countstr(s, c) + 1;
-	ptr = (char **)malloc(size * sizeof(char));
-	if (!ptr)
-		return (0);
-	while (s[i] != '\0')
+	ft_initiate_vars(&i, &j, &s_word);
+	res = ft_calloc((word_count(s, c) + 1), sizeof(char *));
+	if (!res)
+		return (NULL);
+	while (i <= ft_strlen(s))
 	{
-		if ()
-
+		if (s[i] != c && s_word < 0)
+			s_word = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && s_word >= 0)
+		{
+			res[j] = fill_word(s, s_word, i);
+			if (!(res[j]))
+				return (ft_free(res, j));
+			s_word = -1;
+			j++;
+		}
+		i++;
 	}
-
+	return (res);
 }
 
-static	int	countstr(char const *s, char c)
+static	void	ft_initiate_vars(size_t *i, int *j, int *s_word)
 {
-	int	count;
+	*i = 0;
+	*j = 0;
+	*s_word = -1;
+}
+
+static	void	*ft_free(char **strs, int count)
+{
 	int	i;
 
-	count = 0;
 	i = 0;
-	while (s[i] != '\0')
+	while (i < count)
 	{
-		if (s[i] == c)
-			count++;
+		free(strs[i]);
 		i++;
+	}
+	free(strs);
+	return (NULL);
+}
+
+static	char	*fill_word(const char *str, int start, int end)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc((end - start + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	while (start < end)
+	{
+		word[i] = str[start];
+		i++;
+		start++;
+	}
+	word[i] = 0;
+	return (word);
+}
+
+static	int	word_count(const char *str, char c)
+{
+	int	count;
+	int	x;
+
+	count = 0;
+	x = 0;
+	while (*str)
+	{
+		if (*str != c && x == 0)
+		{
+			x = 1;
+			count++;
+		}
+		else if (*str == c)
+			x = 0;
+		str++;
 	}
 	return (count);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hraad <hraad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 14:37:43 by hraad             #+#    #+#             */
-/*   Updated: 2024/08/22 01:56:17 by hraad            ###   ########.fr       */
+/*   Updated: 2024/08/25 00:17:42 by hraad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,21 @@ char		*ft_substr(const char *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strtrim(char const *s1, char const *set);
 char		*ft_itoa(int n);
+char		**ft_split(char const *s, char c);
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void		ft_striteri(char *s, void (*f)(unsigned int, char *));
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+t_list		*ft_lstnew(void *content);
+void		ft_lstadd_front(t_list **lst, t_list *new);
+int			ft_lstsize(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
+void		ft_lstadd_back(t_list **lst, t_list *new);
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstclear(t_list **lst, void (*del)(void *));
+void		ft_lstiter(t_list *lst, void (*f)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
