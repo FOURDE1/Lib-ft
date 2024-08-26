2b2bf182fee166ec67754eb81fdb38141f9bf5e3
Fourde <hossienraad321@gmail.com>
2024-08-26 03:02:44 +0300

    fully working man+bonus

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,32 +6,38 @@
 /*   By: hraad <hraad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 00:04:40 by hraad             #+#    #+#             */
-/*   Updated: 2024/08/25 00:33:14 by hraad            ###   ########.fr       */
+/*   Updated: 2024/08/26 03:00:40 by hraad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static t_list	*ft_lstnew_protected(void *content, void (*del)(void *))
+{
+	t_list	*new_elem;
+
+	new_elem = ft_lstnew(content);
+	if (!new_elem)
+		del(content);
+	return (new_elem);
+}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
 	t_list	*new_elem;
 
-	if (!lst || !f)
-		return (NULL);
-	if (!(new_elem = ft_lstnew(f(lst->content))))
-		return (NULL);
-	new_lst = new_elem;
-	lst = lst->next;
+	new_lst = NULL;
 	while (lst)
 	{
-		if (!(new_elem = ft_lstnew(f(lst->content))))
+		new_elem = ft_lstnew_protected(f(lst->content), del);
+		if (!new_elem)
 		{
 			ft_lstclear(&new_lst, del);
-			break ;
+			return (NULL);
 		}
-		lst = lst->next;
 		ft_lstadd_back(&new_lst, new_elem);
+		lst = lst->next;
 	}
 	return (new_lst);
 }
